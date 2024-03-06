0a2e2b6035d543f5fef3534801f92663e228f061
emilmv <emilnm@code.edu.az>
2024-03-06 07:48:29 +0400

    removed using lines

--- Hometask AccessModifiersPart2/Models/City.cs
+++ Hometask AccessModifiersPart2/Models/City.cs
@@ -1,10 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Runtime;
-using System.Text;
-using System.Threading.Tasks;
-
+﻿
 namespace Hometask_AccessModifiersPart2.Models
 {
     internal class City
