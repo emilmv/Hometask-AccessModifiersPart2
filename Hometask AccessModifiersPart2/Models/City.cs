5800d22a315c701e65b9783355165a1ad6d33f00
emilmv <emilnm@code.edu.az>
2024-03-06 07:45:11 +0400

    Add project files.

---
+++ Hometask AccessModifiersPart2/Models/City.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Hometask_AccessModifiersPart2.Models
+{
+    internal class City
+    {
+        private string _name;
+        public string Name 
+        { 
+            get=> _name;
+            set
+            {
+                if (Name.Length > 3 && Name.Length > 20) return;
+                foreach (char c in Name)
+                    if (!char.IsLetter(c)) return;
+                _name = value;
+            }
+        }
+        private int _population;
+        public int Population
+        {
+            get => _population;
+            set
+            {
+                if (Population < 0) return;
+            }
+        }
+    }
+    internal class  ChinaCity:City
+    {
+        
+
+    }
+}
